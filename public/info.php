19da7f798440c93a1beb29fd59c1dcd8967becb3
nexus-dum <memolapearljam@gmail.com>
2023-05-16 20:07:03 +0200

    hola nacho

--- public/info.php
+++ public/info.php
@@ -2,7 +2,6 @@
 
 phpinfo();
 
-<<<<<<< HEAD
 echo "esto es una prueba";
 
 echo "Inma Almiñana";
@@ -10,11 +9,8 @@ echo "Inma Almiñana";
 
 
 echo"Se me lengua la traba.Ignacio";
-=======
-<<<<<<< HEAD
+
 echo "esto es una prueba";
 echo "Emilio";
-=======
 echo "Esto es una prueba de que Paula me debes 49 euros. Pedro jeje.";
->>>>>>> 2e89ded84cdf71a200542b6352fe47d2499a0041
->>>>>>> b777a5a03fb42000cbced8d5c1e8c64cde934de3
+
